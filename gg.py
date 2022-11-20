536ab4799e233171965b0c219aa644ec5615945d
kubus765 <117770683+kubus765@users.noreply.github.com>
2022-11-20 14:49:34 +0100

    Update gg.py
    Game is fully functional now

--- gg.py
+++ gg.py
@@ -1,1 +1,11 @@
 import random
+def guessing_game():
+    number = random.randint(1,10)
+    print("Guess the number (1-10):")
+    guess = int(input())
+    if guess == number:
+        return "You guessed it! Congratulations!"
+    else:
+        return "Sorry, you didn't guess it, the number was "+str(number)
+while 1:
+    print(guessing_game())
